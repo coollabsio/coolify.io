5f21f68fcd04d95f61437e6bd76e92c300025ba6
Andras Bacsai <andras.bacsai@gmail.com>
2024-02-16 10:17:32 +0100

    update landing page

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,15 +1,14 @@
 <footer class="bg-coolgray-200">
-  <div class="mx-auto overflow-hidden px-6 pt-4 mt-10 lg:px-8 pb-4 text-center" >
+  <div class="mx-auto overflow-hidden px-6 pt-4 mt-10 lg:px-8 pb-4 text-center">
     <nav
       class="columns-2 text-cen ter sm:flex sm:justify-center sm:space-x-12"
       aria-label="Footer"
     >
-      <div> 
-        <a href="https://coollabs.io/products" class="text-sm"
-          >Other Products</a
+      <div>
+        <a href="https://coollabs.io/products" class="text-sm">Other Products</a
         >
       </div>
-      <div > 
+      <div>
         <a href="https://coolify.io/docs/contact" class="text-sm leading-6"
           >Contact</a
         >
@@ -40,6 +39,7 @@
           viewBox="0 0 24 24"
           aria-hidden="true"
         >
+          <title>Github</title>
           <path
             fill-rule="evenodd"
             d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
@@ -55,6 +55,7 @@
           viewBox="0 0 256 256"
           xmlns="http://www.w3.org/2000/svg"
         >
+          <title>Discord</title>
           <g fill="none">
             <rect width="256" height="256" fill="#5865F2" rx="60" />
             <g clip-path="url(#skillIconsDiscord0)">
@@ -71,9 +72,9 @@
           </g>
         </svg>
       </a>
-    </div> 
+    </div>
     <p class="mt-10 text-center text-sm">
-      Made by <a 
+      Made by <a
         class="underline text-warning"
         href="https://twitter.com/heyandras"
         target="_blank">Andras Bacsai</a
--- src/components/Index/Features.svelte
+++ src/components/Index/Features.svelte
@@ -26,7 +26,7 @@
       </svg>
     </div>
   </Feature>
-  <Feature title="Server Automations">
+  <Feature title="Push to Deploy">
     <div slot="icon">
       <svg
         width="512"
@@ -37,26 +37,23 @@
       >
         <g
           fill="none"
-          stroke="white"
+          stroke="currentColor"
           stroke-linecap="round"
           stroke-linejoin="round"
           stroke-width="2"
         >
           <path
-            d="M7 7h10a2 2 0 0 1 2 2v1l1 1v3l-1 1v3a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-3l-1-1v-3l1-1V9a2 2 0 0 1 2-2zm3 9h4"
+            d="M4 18a2 2 0 1 0 4 0a2 2 0 1 0-4 0M4 6a2 2 0 1 0 4 0a2 2 0 1 0-4 0m12 12a2 2 0 1 0 4 0a2 2 0 1 0-4 0M6 8v8"
           />
-          <circle cx="8.5" cy="11.5" r=".5" fill="#000000" />
-          <circle cx="15.5" cy="11.5" r=".5" fill="#000000" />
-          <path d="M9 7L8 3m7 4l1-4" />
+          <path d="M11 6h5a2 2 0 0 1 2 2v8" />
+          <path d="m14 9l-3-3l3-3" />
         </g>
       </svg>
     </div>
-    <div slot="main-text" class="">
-      After connecting your server, Coolify will take over management and handle
-      various administrative tasks for you.<br /><br />You also have the option
-      to create your own scripts to automate server processes<span
-        class="text-warning">*</span
-      >.
+    <div slot="main-text">
+      Today, Git integration is the standard. <br /><br />We offer integration
+      with hosted platforms such as GitHub, GitLab, Bitbucket, Gitea and more (even
+      self-hosted).
     </div>
   </Feature>
   <Feature title="No Vendor Lock-in">
@@ -90,6 +87,37 @@
       even if you choose to stop using Coolify (oh nooo).
     </div>
   </Feature>
+  <Feature title="Server Automations">
+    <div slot="icon">
+      <svg
+        width="512"
+        height="512"
+        class="icon"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g
+          fill="none"
+          stroke="white"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+        >
+          <path
+            d="M7 7h10a2 2 0 0 1 2 2v1l1 1v3l-1 1v3a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-3l-1-1v-3l1-1V9a2 2 0 0 1 2-2zm3 9h4"
+          />
+          <circle cx="8.5" cy="11.5" r=".5" fill="#000000" />
+          <circle cx="15.5" cy="11.5" r=".5" fill="#000000" />
+          <path d="M9 7L8 3m7 4l1-4" />
+        </g>
+      </svg>
+    </div>
+    <div slot="main-text" class="">
+      After connecting your server, Coolify will take over management and handle
+      various administrative tasks for you.
+    </div>
+  </Feature>
+ 
   <Feature title="Monitoring">
     <div slot="icon">
       <svg
@@ -114,6 +142,33 @@
       Coolify will monitor your set servers and deployed resources
       automatically.<br /><br />It will alert you through your preferred
       channels such as Discord, Telegram, and email.
+    </div>
+  </Feature>
+  <Feature title="Multiple Deployment Endpoints">
+    <div slot="icon">
+      <svg
+        width="512"
+        height="512"
+        class="icon"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+        >
+          <path
+            d="M6 4h10l4 4v10a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2"
+          />
+          <path d="M10 14a2 2 0 1 0 4 0a2 2 0 1 0-4 0m4-10v4H8V4" />
+        </g>
+      </svg>
+    </div>
+    <div slot="main-text">
+      You can deploy your resources to a single server, multiple servers, or Docker Swarm clusters seamlessly, based on your needs. <br><br>(Kubernetes support is coming soon!)
     </div>
   </Feature>
   <Feature title="Automatic Backups">
@@ -145,7 +200,7 @@
       with just a few clicks.
     </div>
   </Feature>
-  <Feature title="Powerful API">
+  <Feature title="Powerful API / Webhooks">
     <div slot="icon">
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -165,39 +220,10 @@
     <div slot="main-text">
       Programmatically deploy, query, and manage your servers and resources.
       <br /><br />Integrate with your CI/CD pipelines or build your own custom
-      integrations.<span class="text-warning">*</span>
+      integrations, with Github Actions, Gitlab CI, Bitbucket Pipelines, or basically any CI/CD tool.
     </div>
   </Feature>
-  <Feature title="Push to Deploy">
-    <div slot="icon">
-      <svg
-        width="512"
-        height="512"
-        class="icon"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <g
-          fill="none"
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-        >
-          <path
-            d="M4 18a2 2 0 1 0 4 0a2 2 0 1 0-4 0M4 6a2 2 0 1 0 4 0a2 2 0 1 0-4 0m12 12a2 2 0 1 0 4 0a2 2 0 1 0-4 0M6 8v8"
-          />
-          <path d="M11 6h5a2 2 0 0 1 2 2v8" />
-          <path d="m14 9l-3-3l3-3" />
-        </g>
-      </svg>
-    </div>
-    <div slot="main-text">
-      Today, Git integration is the standard. <br /><br />We offer integration
-      with hosted platforms such as GitHub, GitLab, Bitbucket, Gitea and more (even
-      self-hosted).
-    </div>
-  </Feature>
+ 
   <Feature title="Pull Request Deployments">
     <div slot="icon">
       <svg
@@ -222,8 +248,4 @@
       review contributions and speed up your teamwork!
     </div>
   </Feature>
-</div>
-<div class="pt-20 text-xs px-10">
-  <span class="text-warning">*</span> Some features are work in progress and will
-  be available soon.
 </div>
