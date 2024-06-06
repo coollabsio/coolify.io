51cbb0a534bd15961822a52a936c2d728ce7705a
Andras Bacsai <andras.bacsai@gmail.com>
2024-06-06 15:40:25 +0200

    refactor: Remove unused monitoring and alerting code in Pricing component

--- src/components/Pricing/Plans.svelte
+++ src/components/Pricing/Plans.svelte
@@ -198,21 +198,6 @@
               clip-rule="evenodd"
             />
           </svg>
-          Monitoring and alerting
-        </li>
-        <li class="flex gap-x-3">
-          <svg
-            class="flex-none w-5 h-6 text-warning"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-              clip-rule="evenodd"
-            />
-          </svg>
           Free email notifications
         </li>
         <li class="flex gap-x-3">
@@ -229,21 +214,6 @@
             />
           </svg>
           Support by email
-        </li>
-        <li class="flex gap-x-3">
-          <svg
-            class="flex-none w-5 h-6 text-warning"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-              clip-rule="evenodd"
-            />
-          </svg>
-          ...and more...
         </li>
         <li class="flex font-bold text-white gap-x-3">
           <svg
