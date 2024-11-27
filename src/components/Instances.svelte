b68f4966013889f277db1ca210a1c3f233a61800
Andras Bacsai <andras.bacsai@gmail.com>
2024-11-27 14:48:34 +0100

    Refactor code to format instance count and customer count with locale-specific separators

--- src/components/Instances.svelte
+++ src/components/Instances.svelte
@@ -8,12 +8,12 @@
         headers: { "cool-api-key": "coolify" },
       })
     ).json();
-    instances = count + "+";
+    instances = count.toLocaleString() + "+";
   });
 </script>
 
 <span
   class="px-4 text-neutral-400 sm:text-base text-xs sm:flex-row flex flex-col gap-1  pt-2 sm:justify-center"
-  ><span class="text-warning font-bold font-mono">{instances}</span> self-hosted
+  ><span class="text-warning font-bold font-mono mt-[0.1rem]">{instances}</span> self-hosted
   instances.
 </span>
